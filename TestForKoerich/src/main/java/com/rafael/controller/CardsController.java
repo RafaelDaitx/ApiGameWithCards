57d40054d571161193726bd7b04083e9621dfdad
Rafael Daitx <rafa.daitx.silva@rede.ulbra.br>
2024-08-23 15:36:40 -0300

    Criação da tabela

---
+++ TestForKoerich/src/main/java/com/rafael/controller/CardsController.java
@@ -0,0 +1,17 @@
+package com.rafael.controller;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("cards-service")
+public class CardsController {
+    @GetMapping
+    public List Test(){
+        return null;
+    }
+}
---
+++ TestForKoerich/src/main/resources/db/migration/V1__Create_Vencedores.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE `jogadas` (
+        `id` INT(10) AUTO_INCREMENT PRIMARY KEY,
+        `nomeJogador` longtext NOT NULL ,
+        `cartas` longtext NOT NULL,
+        `pontos` INTEGER NOT NULL,
+        `rodada` INTEGER NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
