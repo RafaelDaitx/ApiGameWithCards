5d0e823a0a4fcccfae98523fd395f7f87a50d4d7
Rafael Daitx <rafa.daitx.silva@rede.ulbra.br>
2024-08-23 15:00:11 -0300

    First Commit

---
+++ TestForKoerich/src/main/java/com/rafael/TesforKoerichApplication.java
@@ -0,0 +1,13 @@
+package com.rafael;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesforKoerichApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesforKoerichApplication.class, args);
+	}
+
+}
