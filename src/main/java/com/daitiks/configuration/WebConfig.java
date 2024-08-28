855e78991faeb08d583cb2a1daed67697d48b96d
Rafael Daitx <rafa.daitx.silva@rede.ulbra.br>
2024-08-28 11:20:11 -0300

    Política de Corts

---
+++ src/main/java/com/daitiks/configuration/WebConfig.java
@@ -0,0 +1,14 @@
+package com.daitiks.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+        //Estou fazendo um front Simples, por isso desta classe
+    }
+}
