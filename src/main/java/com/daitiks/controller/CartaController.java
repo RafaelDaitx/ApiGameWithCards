92d0a9bed06958060ca68d842698f3318f399ee1
Rafael Daitx <rafa.daitx.silva@rede.ulbra.br>
2024-08-28 09:38:15 -0300

    Tratamentos de erros na controller

--- src/main/java/com/daitiks/controller/CartaController.java
+++ src/main/java/com/daitiks/controller/CartaController.java
@@ -2,11 +2,15 @@ package com.daitiks.controller;
 
 import com.daitiks.dto.VencedoresDTO;
 import com.daitiks.service.CartaService;
+import feign.FeignException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Collections;
 import java.util.List;
 
 @RestController
@@ -17,7 +21,15 @@ public class CartaController {
     private CartaService cartaService;
 
     @GetMapping
-    public List<VencedoresDTO> realizarJogada(){
-        return cartaService.realizarJogada();
+    public ResponseEntity<List<VencedoresDTO>> realizarJogada(){
+        try{
+            return ResponseEntity.ok(cartaService.realizarJogada());
+        }catch (FeignException ex){
+            return ResponseEntity.status(HttpStatus.BAD_GATEWAY)
+                    .body(Collections.emptyList());
+        } catch(Exception ex){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(Collections.emptyList());
+        }
     }
 }
